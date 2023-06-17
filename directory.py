bb84d478d875796e54626249a9485f5b8dbe56d4
AlexLojko <a_l_g@tut.by>
2023-06-17 23:31:22 +0300

    add 3punkt

--- directory.py
+++ directory.py
@@ -1,12 +1,37 @@
+new_book = {}
+
+
+def open_file(path='phone.txt'):
+    with open(path, 'r', encoding='UTF-8') as file:
+        data = file.readlines()
+        for con in data:
+            nc = con.strip().split(':')
+            new_book[int(nc[0])] = {'name': nc[1], 'phone': nc[2], 'comment': nc[3]}
+    return new_book
+
+
+def print_contact(book: dict[int, dict]):
+    print('Справочник загружен!!!')
+    print('-' * 50)
+    for i, con in book.items():
+        str_con = f'{i}.{con.get("name")}:{con.get("phone")}:{con.get("comment")}'
+        print(str_con)
+    print('-' * 50)
+
+
+def add_contact(book: dict[int, dict]):
+    book[max(book.keys()) + 1] = {'name': input('Введите ФИО: '),
+                                  'phone': input('Введите номер телефона: '), 'comment':input('Комментарий: ')}
+
 def menu():
     menu = '''Главное меню
-        1. Открыть справочник
-        2. Добавить контакт
-        3. Найти контакт
-        4. Редактировать контакт
-        5. Удалить контакт
-        6. Сохранить справочник
-        7. Выйти'''
+    1. Открыть справочник
+    2. Добавить контакт
+    3. Найти контакт
+    4. Редактировать контакт
+    5. Удалить контакт
+    6. Сохранить справочник
+    7. Выйти'''
     print(menu)
 
     while True:
@@ -15,14 +40,13 @@ def menu():
             return int(choice)
         print('Выберите все таки действие: ')
 
-
 while True:
     choice = menu()
     match choice:
         case 1:
-            pass
+            print_contact(open_file())
         case 2:
-            pass
+            add_contact(open_file())
         case 3:
             pass
         case 4:
